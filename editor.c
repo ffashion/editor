d180ba2c1895f0b6e69bb8ac562950a70ffbfde3
wsl-ffashion <1323579954@qq.com>
2020-11-20 10:56:54 +0800

    Window size, the hard way

--- editor.c
+++ editor.c
@@ -7,7 +7,7 @@
 #include <sys/types.h>
 #include <sys/ioctl.h>
 #include <string.h>
-#define CTRL_KEY(k) ((k) & 0x1f) 
+#define CTRL_KEY(k) ((k) & 0x1f)
 
 /*** data ***/
 struct  editorConfig{
@@ -40,7 +40,6 @@ void enableRawMode(){
     
     
 }
-//读取一个字符
 char editorReadKey(){
     int nread;
     char c;
@@ -61,29 +60,65 @@ void editorProcessKeypress(){
             break;
     }
 }
-int getWindowSize(int *row,int *cols){
+//获取光标位置
+int getCursorPosition(int *rows,int *cols){ 
+    char buf[32];
+    unsigned int i = 0;
+    
+    //读入缓冲区 一直读取 知道读取到R字符
+    if(write(STDIN_FILENO,"\x1b6[",4) != 4) return -1;
+    while (i<sizeof(buf) -1){
+        if(read(STDIN_FILENO,&buf[i],1) !=1 ) break;
+        if(buf[i] == 'R') break;
+        i++;
+    }
+    buf[i] = '\0';
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+    //printf("\r\n&buf[1]: '%s'\r\n", &buf[1]);
+    
+    // printf("\r\n");
+    // char c =0;
+    // while (read(STDIN_FILENO,&c,1) == 1){
+    //     if(iscntrl(c)){
+    //         printf("%d\r\n");
+    //     }else{
+    //         printf("%d ('%c')\r\n", c, c);
+    //     }
+    // }
+    editorReadKey();
+    return -1;
+    
+}
+
+int getWindowSize(int *rows,int *cols){
     struct winsize ws;
     if(ioctl(STDIN_FILENO,TIOCGWINSZ,&ws) == -1 || ws.ws_col == 0){
-        return -1;
+        //将光标移动到右下角
+        //我们写了2个逃逸字符: C command 将光标移动到右侧, B Command将光标移动到下方 . C和B命令有一点的好处是可以阻止越过屏幕边缘
+        //[C Command](http://vt100.net/docs/vt100-ug/chapter3.html#CUF)   [B Command](http://vt100.net/docs/vt100-ug/chapter3.html#CUD)
+        //参数说明移动多少步 这里就是999，999的原因时确保到达右边或者下边
+        //if(write(STDIN_FILENO,'\x1b[999C\x1b[999B',12) != 12) return -1;
+        return getCursorPosition(rows,cols);
     }else{
-        *row = ws.ws_row;
+        *rows = ws.ws_row;
         *cols = ws.ws_col;
         return 0;
     }
 }
-
 /*** output ***/
-//写正确~的次数
 void editorDrawRows(){
     for(int y=0;y<E.screenrows;y++){
         write(STDOUT_FILENO,"~\r\n",3);
     }
 }
-//清屏
 void editorRefreshScreen(){
     write(STDOUT_FILENO,"\x1b[2J",4);
-    //定位光标在左上角,<ESC>[20,20H 将光标定位在20，20处，默认的位置是1，1，所以位于左上角
+    
+    //定位光标在左上角,比如<ESC>[20,20H 将光标定位在20，20处，默认的位置是1，1，所以位于左上角
     write(STDOUT_FILENO,"\x1b[H",3); 
+    
+    //写~
     editorDrawRows();
     //重新定位光标在11处
     write(STDOUT_FILENO,"\x1b[H",3);
@@ -91,6 +126,7 @@ void editorRefreshScreen(){
 
 /*** init ***/
 void initEditor(){
+    //获取窗口大小
     if(getWindowSize(&E.screenrows,&E.screencols) == -1) die("getWindowSize");
 }
 int main(void){
